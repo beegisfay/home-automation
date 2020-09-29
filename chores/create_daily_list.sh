87cf2e9a26dd36499a683709395ac106cc85b4cf
Brannon Fay <brannon.j.fay@gmail.com>
2020-09-28 22:42:25 -0400

    Implemented input verification. Added sourcing of new trello key environment file Loop around existing call to create one list Now support multiple days from cmdline input

--- chores/create_daily_list.sh
+++ chores/create_daily_list.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
 
-. ./fancy_date.sh 5; . new_list.env; python3 create_weekly_lists.py
+if [ $# -ne 2 ]
+then
+    echo "SYNTAX: $0 <<MIN_DAYS_OUT>> <<MAX_DAYS_OUT>>"
+    echo "Where MIN_DAYS_OUT is Today + X Days to start the first daily list"
+    echo "And MAX_DAYS_OUT is the Farthest Day Out for the last daily list"
+    exit 1
+fi
+
+. ./trello_keys
+
+FROM_DAY=$1
+TO_DAY=$2
+
+the_day=${FROM_DAY}
+for i in `echo "for (i=${FROM_DAY};i<=${TO_DAY};i++) i" | bc`
+do
+    echo "Creating list for $i days out"
+    ./fancy_date.sh $i; . ./new_list.env; python3 create_weekly_lists.py
+    echo "List created"
+done
+
+echo "All done"
